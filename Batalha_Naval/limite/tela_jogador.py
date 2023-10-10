15855e279e30407d58812f407134216330bdd72f
Josef Bruseke <josefbruseke@gmail.com>
2023-10-10 17:35:46 -0300

    adiciona metodos

--- Batalha_Naval/limite/tela_jogador.py
+++ Batalha_Naval/limite/tela_jogador.py
@@ -1,3 +1,17 @@
 class TelaJogador:
     def __init__(self) -> None:
         pass
+
+    def recebe_cadastro(self):
+        pass
+
+    def mostra_historico(self):
+        pass
+
+    def mostra_pontuacao(self):
+        pass
+
+    def mostra_ranking(self):
+        pass    
+    
+    
