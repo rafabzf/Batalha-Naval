178ce0c8df61f2e12ebed1ad73b8d064c1e2e39a
Josef Bruseke <josefbruseke@gmail.com>
2023-10-10 09:37:43 -0300

    fix jogo

--- Batalha_Naval/entidade/jogo.py
+++ Batalha_Naval/entidade/jogo.py
@@ -1,28 +1,7 @@
+from jogador import Jogador
 from oceano import Oceano
 
 class Jogo:
-    def __init__(self, nome:str, data_nascimento:str) -> None:
-        self.__nome = nome
-        self.__data_nascimento = data_nascimento
+    def __init__(self) -> None:
+        self.__jogador = Jogador
         self.__oceano = Oceano
-        self.__pontuacao = pontuacao
-        self.__senha = str
-        self.historico = []
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-    
-    @property
-    def data_nascimento(self):
-        return self.__data_nascimento
-
-    @nome.setter
-    def nome(self, data_nascimento):
-        self.__data_nascimento = data_nascimento
-    
-    
