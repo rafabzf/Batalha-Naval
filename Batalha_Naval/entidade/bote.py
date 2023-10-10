cd440f733fbc28d4b68ccf8930e31054fd7401e1
Rafael Bleyer Zanette Furtado <rafinhabzf@gmail.com>
2023-10-10 14:50:56 -0300

    Criando a classe Bote

---
+++ Batalha_Naval/entidade/bote.py
@@ -0,0 +1,15 @@
+from embarcacao import Embarcao
+
+class Bote(Embarcao):
+    def __init__(self, tamanho: int, nome: str, posicoes: int, num_coletes: int):
+        super().__init__(tamanho, nome, posicoes)
+        self.__num_coletes = num_coletes
+        
+    @property
+    def num_coletes(self):
+        return self.__num_coletes
+    
+    @num_coletes.setter
+    def num_coletes(self, num_coletes):
+        self.__num_coletes = num_coletes
+        
