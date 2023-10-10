a20ce69517b0157cc0a5ea6d789f89b29d173c89
Josef Bruseke <josefbruseke@gmail.com>
2023-10-10 10:57:12 -0300

    fix construtor

--- Batalha_Naval/controle/controlador_jogador.py
+++ Batalha_Naval/controle/controlador_jogador.py
@@ -1,3 +1,27 @@
+from limite.tela_jogador import TelaJogador
+from entidade.jogador import Jogador
+
+
 class ControladorJogador:
-    def __init__(self) -> None:
+    def __init__(self, nome, data_nascimento):
+        self.__jogadores = jogadores
+        self.__controlador_sistema = controlador_sistema
+
+    def cadastra_jogador(self):
+        pass
+
+    def altera_cadastro(self):
+        self.lista_jogadores()
+        
+
+    def remove_jogador(self):
+        pass
+
+    def adiciona_jogador_ranking(self):
+        pass
+
+    def lista_jogadores(self):
+        pass
+
+    def busca_jogador(self, usuario, senha):
         pass
