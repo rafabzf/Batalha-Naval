749f507bb9c634ccce94546ae01313f079cfffc7
Josef Bruseke <josefbruseke@gmail.com>
2023-10-11 13:49:47 -0300

    fix bug

---
+++ Batalha_Naval/entidade/fragata.py
@@ -0,0 +1,14 @@
+from embarcacao import Embarcao
+
+class Fragata(Embarcao):
+    def __init__(self, tamanho: int, nome: str, posicoes: int, num_canhoes: int):
+        super().__init__(tamanho, nome, posicoes)
+        self.__num_canhoes = num_canhoes
+        
+    @property
+    def num_canhoes(self):
+        return self.__num_canhoes
+    
+    @num_canhoes.setter
+    def num_canhoes(self, num_canhoes):
+        self.__num_canhoes = num_canhoes
---
+++ Batalha_Naval/entidade/submarino.py
@@ -0,0 +1,14 @@
+from embarcacao import Embarcao
+
+class Submarino(Embarcao):
+    def __init__(self, tamanho: int, nome: str, posicoes: int, profundidade: int):
+        super().__init__(tamanho, nome, posicoes)
+        self.__profundidade = profundidade
+        
+    @property 
+    def profundidade(self):
+        return self.__profundidade
+    
+    @profundidade.setter
+    def profundidade(self, profundidade):
+        self.__profundidade = profundidade
