2ea3ade8c151a017ba7aef5ed594e244418b97d9
Rafael Bleyer Zanette Furtado <rafinhabzf@gmail.com>
2023-10-13 21:48:04 -0300

    Começando a desenvolver a tela de embarcação

---
+++ Batalha_Naval/limite/tela_embarcacao.py
@@ -0,0 +1,9 @@
+from entidade.oceano import Oceano
+
+class TelaEmbarcacao:
+    def mostra_embarcacoes(self):
+       embarcacoes = Oceano.embarcacoes
+       return embarcacoes 
+    
+    def recebe_posicao_embarcacao(self):
+        pass
