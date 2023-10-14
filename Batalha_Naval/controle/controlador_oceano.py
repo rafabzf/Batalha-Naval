2ace5122fd88693d095211dd064bb9f61289a5d1
Rafael Bleyer Zanette Furtado <rafinhabzf@gmail.com>
2023-10-14 18:04:01 -0300

    Implementando método para criar oceano

--- Batalha_Naval/controle/controlador_oceano.py
+++ Batalha_Naval/controle/controlador_oceano.py
@@ -2,15 +2,17 @@ from limite.tela_embarcacao import TelaEmbarcacao
 from limite.tela_oceano import TelaOceano
 
 class ControladorOceano:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.__tela_embarcacao = TelaEmbarcacao()
+        self.__tela_oceano = TelaOceano
     
     def cria_oceano(self):
-        pass
+        tamanho_oceano = self.__tela_oceano.recebe_tamanho()
+        oceano = [['O' for _ in range(tamanho_oceano)] for _ in range(tamanho_oceano)]
+        return oceano
     
     def cria_oceano_computador(self):
         pass
     
     def adiciona_embarcacao_oceano(self):
         pass
-
