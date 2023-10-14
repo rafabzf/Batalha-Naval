c5755eaa5e624b514b6afb778e8b30ea2eacb0be
Rafael Bleyer Zanette Furtado <rafinhabzf@gmail.com>
2023-10-13 21:27:11 -0300

    Começando a desenvolver a tela do oceano

---
+++ Batalha_Naval/limite/tela_oceano.py
@@ -0,0 +1,11 @@
+class TelaOceano:
+    def recebe_tamanho(self):
+        print("-----TAMANHO DO OCEANO-----")
+        tamanho = int(input("Informe o tamanho do oceano da partida: "))
+        return tamanho
+        
+    def mostra_oceano(self):
+        pass
+    
+    def mostra_mensagem(self):
+        pass
