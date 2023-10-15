3248e97526fd08eca86391c4f45f2f9b57fa6c92
Josef Bruseke <josefbruseke@gmail.com>
2023-10-15 20:48:16 -0300

    rename methodes names

--- controle/controlador_sistema.py
+++ controle/controlador_sistema.py
@@ -1,13 +1,13 @@
 from limite.tela_sistema import TelaSistema
 from controle.controlador_jogador import ControladorJogador
-#from controle.controlador_jogo import ControladorJogo
+from controle.controlador_jogo import ControladorJogo
 #from controle.controlador_oceano import ControladorOceano
 
 
 class ControladorSistema:
     def __init__(self) -> None:
-        self.__controlador_jogador = ControladorJogador(self)
-        #self.__controlador_jogo = ControladorJogo(self)
+        self.__controlador_jogador = ControladorJogador()
+        self.__controlador_jogo = ControladorJogo()
         #self.__controlador_oceano = ControladorOceano(self)
         self.__tela_sistema = TelaSistema()
 
@@ -15,21 +15,17 @@ class ControladorSistema:
         self.__controlador_jogador.cadastra_jogador()
         self.abre_opcoes()
 
-    def faz_login(self):
-        pass
+    def inicia_login(self):
+        self.__controlador_jogo.faz_login()
         
-
     def mostra_ranking(self):
-        pass
-
-    def inicia_jogo(self):
         pass
 
     def inicializa_sistema(self):
         self.abre_opcoes()
 
     def abre_opcoes(self):
-        lista_opcoes = {1: self.faz_login, 
+        lista_opcoes = {1: self.inicia_login, 
                         2: self.inicia_cadastro,
                         0: self.encerra_sistema}
         opcao_selecionada = self.__tela_sistema.mostra_opcoes()
