e63352c0c40fdfdbd2ba87ff92cad2873f447050
Josef Bruseke <josefbruseke@gmail.com>
2023-10-15 20:50:32 -0300

    add recebe_login, mostra_mensagem

--- limite/tela_jogo.py
+++ limite/tela_jogo.py
@@ -1,6 +1,10 @@
 class TelaJogo:
-    def __init__(self) -> None:
-        pass
+
+    def recebe_login(self):
+        print("------LOGIN-------")
+        recebe_nome = input("Digite seu nome: ")
+        recebe_senha = input("Digite sua senha: ")
+        return{"recebe_nome": recebe_nome, "recebe_senha": recebe_senha}
 
     def mostra_resultado_jogo(self):
         pass
@@ -8,5 +12,5 @@ class TelaJogo:
     def mostra_pontuacao_jogada(self):
         pass
 
-
-
+    def mostra_mensagem(self, msg):
+        print(msg)
