8388310e33879a1a8e97abb17ff45ebcb8a30b15
Josef Bruseke <josefbruseke@gmail.com>
2023-10-15 20:55:51 -0300

    change directory

---
+++ entidade/embarcacao.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+
+class Embarcao(ABC):
+    @abstractmethod
+    def __init__(self, tamanho: int, nome: str, posicoes: int):
+        self.__tamanho = tamanho
+        self.__nome: nome
+        self.__posicoes: posicoes
+        
+    @property
+    def tamanho(self):
+        return self.__tamanho
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def posicoes(self):
+        return self.__posicoes
+    
+    @tamanho.setter
+    def tamanho(self, tamanho: int):
+        self.__tamanho = tamanho
+        
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+    
+    @posicoes.setter
+    def posicoes(self, posicoes: int):
+        self.__posicoes = posicoes
