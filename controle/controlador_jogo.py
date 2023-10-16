86da4afbeea1ca51e242bf359e18612cc135278c
Josef Bruseke <josefbruseke@gmail.com>
2023-10-16 13:48:26 -0300

    Update controlador_jogo.py

--- controle/controlador_jogo.py
+++ controle/controlador_jogo.py
@@ -4,7 +4,10 @@ from controle.controlador_sistema import ControladorSistema
 
 class ControladorJogo:
     def __init__(self) -> None:
+<<<<<<< Updated upstream
         pass
+=======
+>>>>>>> Stashed changes
         self.__controlador_sistema = ControladorSistema(self)
         self.__tela_jogo = TelaJogo()
 
@@ -19,10 +22,32 @@ class ControladorJogo:
             self.faz_login(self)
 
     def inicia_jogo(self):
+<<<<<<< Updated upstream
         try:
             self.__tela_jogo.mostra_mensagem("Jogo iniciado!")
         except:
             self.__tela_jogo.mostra_mensagem("Jogo não iniciado!")
+=======
+        self.abre_opcoes()
+
+    def inicia_partida(self):
+        pass
+
+    def voltar(self):
+        pass
+
+    def mostra_ranking(self):
+        pass
+
+    def abre_opcoes(self):
+        lista_opcoes = {1: self.inicia_partida, 
+                        2: self.mostra_ranking,
+                        0: self.voltar}
+        opcao_selecionada = self.__tela_sistema.mostra_opcoes()
+        funcao_escolhida = lista_opcoes[opcao_selecionada]
+        funcao_escolhida()
+
+>>>>>>> Stashed changes
 
     def reliza_jogada(self):
         pass
