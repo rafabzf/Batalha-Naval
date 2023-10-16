65e262f29803e905828efb48ee7f88d75d032c3c
Rafael Bleyer Zanette Furtado <rafinhabzf@gmail.com>
2023-10-16 16:26:16 -0300

    Arrumando métodos para criar os oceanos

--- limite/tela_oceano.py
+++ limite/tela_oceano.py
@@ -1,14 +1,23 @@
 class TelaOceano:
+    '''def mostra_opcoes(self):
+        print("------Tela Oceano------")
+        print("Selecione a opção desejada")
+        print("1 - Fazer Login")
+        print("2 - Fazer Cadastro")
+        print("0 - Encerrar o sistema")
+        opcao = int(input("Escolha a opção: "))
+        return opcao'''
+    
     def recebe_tamanho(self):
         print("-----TAMANHO DO OCEANO-----")
-        tamanho = int(input("Informe o tamanho do oceano da partida: "))
-        return tamanho
-        
-    def mostra_oceano_jogador(self):
-        pass
-    
-    def mostra_oceano_computador(self):
-        pass
-    
-    def mostra_mensagem(self):
-        print("Oceano criado com sucesso!")
+        while True:
+            tamanho = int(input("Informe o tamanho do oceano da partida: "))
+            if tamanho < 6:
+                print('Oceano pequeno demais, insira um valor entre 6 e 9')
+            elif tamanho > 9:
+                print('Oceano grande demais, insira um valor entre 6 e 9')
+            else:
+                return tamanho
+
+    def mostra_mensagem(self, msg):
+        print(msg)
