93bbfc2bbcc87e86e082fb35490d08eb08a86456
Josef Bruseke <josefbruseke@gmail.com>
2023-10-16 13:36:17 -0300

    add try/expect

--- controle/controlador_jogador.py
+++ controle/controlador_jogador.py
@@ -5,15 +5,19 @@ from entidade.jogador import Jogador
 class ControladorJogador:
     def __init__(self):
         self.__jogadores = []
+        #self.__ranking = []
         self.__tela_jogador = TelaJogador()
 
     def cadastra_jogador(self):
         dados_cadastro = self.__tela_jogador.recebe_cadastro()
-        jogador = Jogador(
-            dados_cadastro["nome"], dados_cadastro["data_nascimento"], dados_cadastro["senha"])
-        self.__jogadores.append(jogador)
-        self.__tela_jogador.mostra_mensagem("\nCadastro realizado com sucesso!")
-        self.__tela_jogador.mostra_mensagem("Faça Login para jogar! \n")
+        try:
+            jogador = Jogador(
+                dados_cadastro["nome"], dados_cadastro["data_nascimento"], dados_cadastro["senha"])
+            self.__jogadores.append(jogador)
+            self.__tela_jogador.mostra_mensagem("\nCadastro realizado com sucesso!")
+            self.__tela_jogador.mostra_mensagem("Faça Login para jogar! \n")
+        except:
+            self.__tela_jogador.mostra_mensagem("ERRO: Jogador não cadastrado!")
 
     def altera_cadastro(self):
         pass
@@ -21,8 +25,9 @@ class ControladorJogador:
     def remove_jogador(self):
         pass
 
-    def adiciona_jogador_ranking(self):
-        pass
+    def ordena_ranking(self):
+        #Use o método `sort` para ordenar com base na pontuação e, em seguida, no nome.
+        self.lista_jogadores.sort(key=lambda jogador: (jogador.pontuacao, jogador.nome))
 
     def lista_jogadores(self):
 
@@ -30,8 +35,10 @@ class ControladorJogador:
             self.__tela_jogador.mostra_mensagem({"nome": jogador.nome})
 
     def estah_cadastrado(self, nome, senha):
-        for jogador in self.__jogadores:
-            if jogador.nome == nome and jogador.senha == senha:
-                return True
-            else:
-                return False
+        try:
+            for jogador in self.__jogadores:
+                if jogador.nome == nome and jogador.senha == senha:
+                    return True
+            return False
+        except: 
+            self.__tela_jogador.mostra_mensagem("Falha ao verificar login")
--- controle/controlador_jogo.py
+++ controle/controlador_jogo.py
@@ -1,23 +1,28 @@
-# from entidade.jogo import Jogo
+#from entidade.jogo import Jogo
 from limite.tela_jogo import TelaJogo
 from controle.controlador_sistema import ControladorSistema
 
 class ControladorJogo:
     def __init__(self) -> None:
-        self.__controlador_sistema = ControladorSistema()
+        pass
+        self.__controlador_sistema = ControladorSistema(self)
         self.__tela_jogo = TelaJogo()
 
     def faz_login(self):
         dados_login = self.__tela_jogo.recebe_login()
         nome, senha = dados_login["recebe_nome"], dados_login["recebe_senha"]
-        if self.__controlador_sistema.__controlador_jogador.estah_cadastrado(nome, senha):
+        try:
+            self.__controlador_sistema.__controlador_jogador.estah_cadastrado(nome, senha)
             self.inicia_jogo()
-        else:
+        except:
             self.__tela_jogo.mostra_mensagem("Jogador não encontrado!")
             self.faz_login(self)
 
     def inicia_jogo(self):
-        self.__tela_jogo.mostra_mensagem("Jogo iniciado!")
+        try:
+            self.__tela_jogo.mostra_mensagem("Jogo iniciado!")
+        except:
+            self.__tela_jogo.mostra_mensagem("Jogo não iniciado!")
 
     def reliza_jogada(self):
         pass
