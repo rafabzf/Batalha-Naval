59070f1c4593883455bb4f47ef2906d5b1e1d8da
Josef Bruseke <josefbruseke@gmail.com>
2023-10-25 09:35:06 -0300

    fix typo

--- limite/tela_jogador.py
+++ limite/tela_jogador.py
@@ -2,20 +2,17 @@ class TelaJogador:
     def recebe_cadastro(self):
         print("------CADASTRO------")
         nome = input("Digite seu nome: ")
-        data_nacimento = input("Digite sua data de nascimento: ")
+        data_nascimento = input("Digite sua data de nascimento: ")
         senha = input("Digite sua data de senha: ")
-        return{"nome": nome, "data_nascimento": data_nacimento, "senha": senha}
+        return {"nome": nome, "data_nascimento": data_nascimento, "senha": senha}
 
     def mostra_historico(self):
         pass
 
     def mostra_pontuacao(self):
         pass
-
-    def mostra_ranking(self):
-        pass
     
-    def mostra_lista_jodares(self):
+    def mostra_lista_jogadores(self):
         pass
  
     def mostra_mensagem(self, msg):
