edf758e0d8970deda1114c0da13e1f4ba65fb165
Josef Bruseke <josefbruseke@gmail.com>
2023-10-25 10:30:29 -0300

    change methods

--- controle/controlador_oceano.py
+++ controle/controlador_oceano.py
@@ -6,16 +6,19 @@ class ControladorOceano:
         self.__tela_embarcacao = TelaEmbarcacao()
         self.__tela_oceano = TelaOceano()
         self.__controlador_sistema = controlador_sistema
+        self.__tamanho_oceano = None
     
+    
+    def armazena_tamanho_oceano(self):
+        self.__tamanho_oceano = self.__tela_oceano.recebe_tamanho()
+
+
     def cria_oceano(self):
-        tamanho_oceano = self.__tela_oceano.recebe_tamanho()
-        oceano = [['O' for _ in range(tamanho_oceano)] for _ in range(tamanho_oceano)]
+        oceano = [['O' for _ in range(self.__tamanho_oceano)] for _ in range(self.__tamanho_oceano)]
         return oceano
     
     def cria_oceano_computador(self):
-        tamanho_oceano = self.__tela_oceano.recebe_tamanho()
-        oceano_computador = [['O' for _ in range(tamanho_oceano)] for _ in range(tamanho_oceano)]
-        return oceano_computador
+        self.cria_oceano()
     
     def adiciona_embarcacao_oceano(self):
         pass
