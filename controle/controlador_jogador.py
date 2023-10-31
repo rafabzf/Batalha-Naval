1867492657ee95f358506612477dc2a9cfe9d45d
Josef Bruseke <josefbruseke@gmail.com>
2023-10-31 16:09:08 -0300

    treat exception

--- controle/controlador_jogador.py
+++ controle/controlador_jogador.py
@@ -1,27 +1,37 @@
 from limite.tela_jogador import TelaJogador
 from entidade.jogador import Jogador
+from controle.controlador_execessao import Controlador_excessao
+import datetime
 
 
 class ControladorJogador:
     def __init__(self, controlador_sistema):
         self.__controlador_sistema = controlador_sistema
         self.__tela_jogador = TelaJogador()
+        self.__controlador_execessao = Controlador_excessao()
         self.__jogadores = []
 
     def cadastra_jogador(self):
         dados_jogador = self.__tela_jogador.recebe_cadastro()
 
+        try:
+            data_nascimento = datetime.datetime.strptime(dados_jogador["data_nascimento"], "%d/%m/%Y")
+        except ValueError:
+            self.__tela_jogador.mostra_mensagem("Formato de data de nascimento inválido. Tente novamente.")
+            self.cadastra_jogador()
+            return
+
         for jogador in self.__jogadores:
-            if dados_jogador["nome"] == jogador.nome and dados_jogador["data_nascimento"] == jogador.data_nascimento:
+            if dados_jogador["nome"] == jogador.nome and data_nascimento == jogador.data_nascimento:
                 self.__tela_jogador.mostra_mensagem("Jogador já cadastrado!")
                 self.__controlador_sistema.abre_opcoes()
                 return
 
-        jogador = Jogador(dados_jogador["nome"], dados_jogador["data_nascimento"], dados_jogador["senha"], pontuacao=0)
+        jogador = Jogador(dados_jogador["nome"], data_nascimento, dados_jogador["senha"], pontuacao=0)
         self.__jogadores.append(jogador)
         self.__tela_jogador.mostra_mensagem("\nCadastro realizado com sucesso!")
         self.__tela_jogador.mostra_mensagem("Faça Login para jogar! \n")
-
+        
     def altera_cadastro(self):
         pass
 
