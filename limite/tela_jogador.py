a7ab1c4619e0df3bc40680d8c6bb8d79a07ae8c2
Josef Bruseke <josefbruseke@gmail.com>
2023-10-31 16:10:24 -0300

    fix

--- limite/tela_jogador.py
+++ limite/tela_jogador.py
@@ -2,7 +2,7 @@ class TelaJogador:
     def recebe_cadastro(self):
         print("------CADASTRO------")
         nome = input("Digite seu nome: ")
-        data_nascimento = input("Digite sua data de nascimento: ")
+        data_nascimento = input("Digite sua data de nascimento (DD/MM/AAAA): ")
         senha = input("Digite sua data de senha: ")
         return {"nome": nome, "data_nascimento": data_nascimento, "senha": senha}
 
