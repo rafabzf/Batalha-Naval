3ec3737831b3a01052239c58f7090719c548fdbe
Josef Bruseke <josefbruseke@gmail.com>
2023-11-15 11:22:11 -0300

    create class

---
+++ entidade/jogador_dao.py
@@ -0,0 +1,15 @@
+from entidade.dao import DAO
+from entidade.jogador import Jogador
+
+class JogadorDAO(DAO):
+    def __init__(self):
+        super().__init__('jogador.pkl')
+
+    def add(self, jogador:Jogador):
+        return super().add(jogador.nome, jogador)
+    
+    def get(self, nome:str):
+        return super().get(nome)
+    
+    def remove(self, jogador):
+        return super().remove(jogador.nome)
