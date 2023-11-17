0d99e33bb5741131100a0f0d389553dbfc512827
Josef Bruseke <josefbruseke@gmail.com>
2023-11-17 00:16:36 -0300

    change menus

--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -2,9 +2,8 @@ class TelaSistema:
     def mostra_opcoes(self):
         print("------MENU INICIAL------")
         print("Selecione a opção desejada")
-        print("1 - Fazer Login")
-        print("2 - Fazer Cadastro")
-        print("3 - Remover Cadastro")
+        print("1 - Login")
+        print("2 - Cadastro")
         print("0 - Encerrar o sistema")
         opcao = int(input("Escolha a opção: "))
         return opcao
